5fba833336fa1ae4675c84f01f4425e1da776c7a
Steven Tobias <stobias123@gmail.com>
2022-10-31 00:42:45 -0700

    finally fixing up setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tank_royal_gym',
+    version='',
+    packages=['lib', 'lib.render', 'lib.bot_api', 'lib.dependency_managers', 'envs', 'test'],
+    package_dir={'': 'tank-royal-gym'},
+    url='',
+    license='',
+    author='steven.tobias',
+    author_email='',
+    description=''
+)
